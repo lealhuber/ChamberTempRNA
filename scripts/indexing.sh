ee2feb3d9254d7f535e156983955edabb670a100
lealhuber <lea.huber@datazug.ch>
2024-03-11 16:01:52 +0100

    modified: more memory

--- scripts/indexing.sh
+++ scripts/indexing.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=ostrich_thermal
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=15G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64g
 #SBATCH --time=2:00:00
 
 # indexing reference and annotation
-STAR --runThreadN 4 --runMode genomeGenerate --genomeDir /faststorage/project/ostrich_thermal/people/leah/ChamberTempRNA/data/reference/reference_indexed \
+STAR --runThreadN 8 --runMode genomeGenerate --genomeDir /faststorage/project/ostrich_thermal/people/leah/ChamberTempRNA/data/reference/reference_indexed \
     --genomeFastaFiles /faststorage/project/ostrich_thermal/people/leah/ChamberTempRNA/data/reference/Struthio_camelus_australis.ASM69896v1.dna.toplevel.fa \
     --sjdbGTFfile /faststorage/project/ostrich_thermal/people/leah/ChamberTempRNA/data/reference/Struthio_camelus_australis.ASM69896v1.106.gtf \
     --sjdbOverhang 149
