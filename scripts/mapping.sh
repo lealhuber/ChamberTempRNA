32eb1366da0dadf7b09053ceef38578a14a273d2
lealhuber <lea.huber@datazug.ch>
2024-03-13 15:18:24 +0100

    updated mapping script for paired-end

--- scripts/mapping.sh
+++ scripts/mapping.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 #SBATCH --account=ostrich_thermal
-#SBATCH --cpus-per-task=12
+#SBATCH --cpus-per-task=10
 #SBATCH --mem=32G
 #SBATCH --time=12:00:00
 
-# run in analysis/testsamples
+# run in analysis/testsamples/mapping and have a symlink to data there? But then there's a path in f...
+# create file with base-filenames without read1 or read2 called filebase
+# adjust for paired-end data!!! Test this on 2 samples first or so
 
-# go through each file seperately for mapping
-for f in *.fq.gz ; do
+## go through each file seperately for mapping
+# define path to data directory
+dat_dir=/faststorage/project/ostrich_thermal/people/leah/ChamberTempRNA/data/illumina_test
+while read filebase
+do
+    echo filebase
+    # assemble and define filenames
+    fq_read1=$dat_dir/${filebase}_read1.fastq.gz
+    fq_read2=$dat_dir/${filebase}_read2.fastq.gz
     # map with STAR with an output BAM 
-    STAR --runThreadN 12 --genomeDir /faststorage/project/ostrich_thermal/people/leah/ChamberTempRNA/data/reference/reference_indexed \
-        --readFilesIn $f --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate \
-        --outFileNamePrefix STARout/$f.
+    STAR --runThreadN 10 --genomeDir /faststorage/project/ostrich_thermal/people/leah/ChamberTempRNA/data/reference/reference_indexed \
+        --readFilesIn $fq_read1 $fq_read2 --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate \
+        --outFileNamePrefix $filebase.
     # makes files sample#.Aligned.sortedByCoord.out.bam
     # Grab all alignments except ones that map to more than 4 locations:
-    samtools view -b -q 1 STARout/$f.Aligned.sortedByCoord.out.bam > STARout/$f.Aligned.sortedByCoord.q1.out.bam
+    samtools view -b -q 1 $filebase.Aligned.sortedByCoord.out.bam > $filebase.Aligned.sortedByCoord.q1.out.bam
     # index because htseq is complaining
-    samtools index -@ 12 STARout/$f.Aligned.sortedByCoord.q1.out.bam # generates BAI-format index Aligned.sortedByCoord.q1.out.bam.bai
+    samtools index -@ 10 $filebase.Aligned.sortedByCoord.q1.out.bam # generates BAI-format index Aligned.sortedByCoord.q1.out.bam.bai
     #Count the reads
-    htseq-count --stranded=no STARout/$f.Aligned.sortedByCoord.q1.out.bam \
-        /faststorage/project/ostrich_thermal/people/leah/broiler_testDGEA/broiler_testdata/reference/ensembl/Gallus_gallus.GRCg6a.106.gtf \
-        > $f.count_matrix.txt ;
+    htseq-count --stranded=no $filebase.Aligned.sortedByCoord.q1.out.bam \
+        /faststorage/project/ostrich_thermal/people/leah/ChamberTempRNA/data/reference/Struthio_camelus_australis.ASM69896v1.106.gtf \
+        > $filebase.count_matrix.txt ;
 done
 
 # make R style file with filenames for DESeqDataSetFromHTSeqCount function
