ac34189e30356112a8ec0d646e887c4e07039a46
lealhuber <lea.huber@datazug.ch>
2024-03-19 08:13:09 +0100

    Updated trimming part

--- scripts/rawreadsQC.sh
+++ scripts/rawreadsQC.sh
@@ -2,16 +2,15 @@
 #SBATCH --account=ostrich_thermal
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32g
-#SBATCH --time=01:00:00
+#SBATCH --time=06:00:00
 
 # run in analysis/testsamples/QCtrimming
-
 # run fastqc on all samples, should make an .html files as report in same folder
 fastqc -t 8 ../../../data/00_fastq/*.fastq.gz
 
 # run trim galore! to trim adaptors (will detect them automatically) and reads with <Q10
 trim_galore --paired --fastqc --gzip --cores 8 -q 10 \
-    -o /faststorage/project/ostrich_thermal/people/leah/ChamberTempRNA/data/illumina_test/trimmed \
-    ../../../data/illumina_test/*.fastq.gz
+    -o /faststorage/project/ostrich_thermal/people/leah/ChamberTempRNA/data/illumina_test/trimmed/ \
+    /faststorage/project/ostrich_thermal/BACKUP/RNAseqTest/00_fastq/*.fastq.gz
 
 exit 0
