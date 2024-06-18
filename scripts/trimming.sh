0df49fabc376299b250bfaaab563d6fe24ff575b
lealhuber <lea.huber@datazug.ch>
2024-06-18 16:39:30 +0200

    Added because it did not work within rawreadsQC bec of minor errors

---
+++ scripts/trimming.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --account=ostrich_thermal
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32g
+#SBATCH --time=12:00:00
+
+# run trim galore! to trim adaptors (will detect them automatically) and reads with <Q10 (good enough for DGE)
+trim_galore --paired --fastqc --gzip --cores 8 -q 10 \
+    -o /faststorage/project/ostrich_thermal/BACKUP/ChamberRNAseq/data/trimmed \
+    /faststorage/project/ostrich_thermal/BACKUP/ChamberRNAseq/data/90-964973806/00_fastq/L[4-9]*.fastq.gz
+# -q 10 : Rachel recommended only light quality trimming of <Q10 (instead of default <Q20)
+trim_galore --paired --fastqc --gzip --cores 8 -q 10 \
+    -o /faststorage/project/ostrich_thermal/BACKUP/ChamberRNAseq/data/trimmed \
+    /faststorage/project/ostrich_thermal/BACKUP/ChamberRNAseq/data/90-964973806/00_fastq/L3[2-9]*.fastq.gz
+# -q 10 : Rachel recommended only light quality trimming of <Q10 (instead of default <Q20)
+trim_galore --paired --fastqc --gzip --cores 8 -q 10 \
+    -o /faststorage/project/ostrich_thermal/BACKUP/ChamberRNAseq/data/trimmed \
+    /faststorage/project/ostrich_thermal/BACKUP/ChamberRNAseq/data/90-964973806/00_fastq/L3_*.fastq.gz
+# -q 10 : Rachel recommended only light quality trimming of <Q10 (instead of default <Q20)
+exit 0
