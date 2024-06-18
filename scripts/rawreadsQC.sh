c63eef279285881d85e1b7fcae9d789336235360
lealhuber <lea.huber@datazug.ch>
2024-06-18 16:36:21 +0200

    Changed folder names for actual chamber temp data

--- scripts/rawreadsQC.sh
+++ scripts/rawreadsQC.sh
@@ -4,13 +4,22 @@
 #SBATCH --mem=32g
 #SBATCH --time=06:00:00
 
-# run in analysis/testsamples/QCtrimming
-# run fastqc on all samples, should make an .html files as report in same folder
-fastqc -t 8 ../../../data/00_fastq/*.fastq.gz
+# rename them, ie remove the _001
+cd /faststorage/project/ostrich_thermal/BACKUP/ChamberRNAseq/data/90-964973806/00_fastq
+for filename in *.fastq.gz; do
+    base=${filename%_001.fastq.gz}
+    mv $filename $base.fastq.gz
+done
 
-# run trim galore! to trim adaptors (will detect them automatically) and reads with <Q10
-trim_galore --paired --fastqc --gzip --cores 8 -q 10 \ 
-    -o /faststorage/project/ostrich_thermal/people/leah/ChamberTempRNA/data/illumina_test/trimmed/ \
-    /faststorage/project/ostrich_thermal/BACKUP/RNAseqTest/00_fastq/*.fastq.gz
+# run in steps/QC
+cd /faststorage/project/ostrich_thermal/people/leah/ChamberTempRNA/steps/QC
+
+# run fastqc on all samples, should make an .html files as report in same folder
+fastqc -t 8 /faststorage/project/ostrich_thermal/BACKUP/ChamberRNAseq/data/90-964973806/00_fastq/*.fastq.gz
+
+# run trim galore! to trim adaptors (will detect them automatically) and reads with <Q10 (good enough for DGE)
+trim_galore --paired --fastqc --gzip --cores 8 -q 10 \
+    -o /faststorage/project/ostrich_thermal/BACKUP/ChamberRNAseq/data/trimmed \
+    /faststorage/project/ostrich_thermal/BACKUP/ChamberRNAseq/data/90-964973806/00_fastq/*.fastq.gz
 # -q 10 : Rachel recommended only light quality trimming of <Q10 (instead of default <Q20)
 exit 0
