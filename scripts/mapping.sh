f9ff10d64778dcd2acac1b9e5e3985fcfe851515
lealhuber <lea.huber@datazug.ch>
2024-06-25 14:47:54 +0200

    removed read counting into separate script, added stats

--- scripts/mapping.sh
+++ scripts/mapping.sh
@@ -2,42 +2,47 @@
 #SBATCH --account=ostrich_thermal
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=32G
-#SBATCH --time=12:00:00
+#SBATCH --time=72:00:00
 
-# run in analysis/testsamples/mapping and have a symlink to data there? But then there's a path in f...
+# run in steps/mapping and have a symlink to data there? But then there's a path in f...
 # create file with base-filenames without read1 or read2 called filebase and run in same directory
 # adjust for paired-end data!!! Test this on 2 samples first or so
 
+# I renamed them all in a previous attempt that otherwise failed with this code:
+# for filename in $dat_dir/*.fq.gz; do
+#     # rename them, ie remove the _val part
+#     baseread=${filename%_val_*.fq.gz}
+#     mv $filename $baseread.trimmed.fq.gz
+
 ## go through each file seperately for mapping
 # define path to data directory
-dat_dir=/faststorage/project/ostrich_thermal/people/leah/ChamberTempRNA/data/illumina_test/trimmed
+dat_dir=/faststorage/project/ostrich_thermal/BACKUP/ChamberRNAseq/data/trimmed
 
-while read filebase
-do
+# want to do everything only once of course!! so only take the R1 file
+for filename in $dat_dir/*_R1.trimmed.fq.gz; do
+    # extract just the sample filebase name without read information
+    namenopath=${filename##*/}
+    filebase=${namenopath%_*}
     echo $filebase
-    # assemble and define filenames
-    fq_read1=$dat_dir/${filebase}_R1.fq.gz
-    fq_read2=$dat_dir/${filebase}_R2.fq.gz
+    # assemble and define filenames for each read pair
+    fq_read1=$dat_dir/${filebase}_R1.trimmed.fq.gz
+    fq_read2=$dat_dir/${filebase}_R2.trimmed.fq.gz
+    # I want to do this only
     # map with STAR with an output BAM 
-    STAR --runThreadN 10 --genomeDir /faststorage/project/ostrich_thermal/people/leah/ChamberTempRNA/data/reference/reference_indexed \
+    STAR --runThreadN 10 --genomeDir /faststorage/project/ostrich_thermal/BACKUP/ostrich_reference/Struthio_camelus_HiC/HiC_indexed \
         --readFilesIn $fq_read1 $fq_read2 --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate \
         --outFileNamePrefix $filebase.
     # makes files sample#.Aligned.sortedByCoord.out.bam
+    echo "Star done"
     # Grab all alignments except ones that map to more than 4 locations:
     samtools view -b -q 1 $filebase.Aligned.sortedByCoord.out.bam > $filebase.Aligned.sortedByCoord.q1.out.bam
     # index because htseq is complaining
     samtools index -@ 10 $filebase.Aligned.sortedByCoord.q1.out.bam # generates BAI-format index Aligned.sortedByCoord.q1.out.bam.bai
-    #Count the reads
-    htseq-count --stranded=no $filebase.Aligned.sortedByCoord.q1.out.bam \
-        /faststorage/project/ostrich_thermal/people/leah/ChamberTempRNA/data/reference/Struthio_camelus_australis.ASM69896v1.106.gtf \
-        > $filebase.count_matrix.txt ;
-done < filebase
 
-# make R style file with filenames for DESeqDataSetFromHTSeqCount function
-ls *count_matrix.txt | sed 's/.*/"&",/' > matrixFileNames.txt
-# same with first part of sample names (ADJUST!!!)
-ls *count_matrix.txt | sed 's/\(B...._rep.\)\(_.*\)/"\1",/' > SampleNames.txt
-# make file with just file names to copy them to local computer
-ls *count_matrix.txt > matrixFileNamesCopy.txt
+    ## this didn't work / was added later
+    # Check coverage, try to infer about library creation and sequencing
+    samtools stats -@ 10 -r /faststorage/project/ostrich_thermal/BACKUP/ostrich_reference/Struthio_camelus_HiC/Struthio_camelus_HiC.fasta $filebase.Aligned.sortedByCoord.out.bam > $filebase.Aligned.sortedByCoord.out.bam.stats
+    plot-bamstats -p plots/ $filebase.Aligned.sortedByCoord.out.bam.stats
+done
 
 exit 0
