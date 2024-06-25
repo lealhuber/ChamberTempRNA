3c19af3aa86fb15d42faca3fd59b4ee0a938996d
lealhuber <lea.huber@datazug.ch>
2024-06-25 14:51:13 +0200

    Script with some RSeQC commands probably broken

---
+++ scripts/SampleMappingQC.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --account=ostrich_thermal
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32G
+#SBATCH --time=18:00:00
+
+# run in mapping
+# QC of mapped samples
+for filename in L5*.out.bam; do
+    # summarising mapping statistics
+    bam_stat.py -i $filename > ../QC/rseqc/${filename}_stats.txt # perhaps also need to specify -q 255 because in star that is the minimum mapq for uniquely mapping read
+
+    # Calculate the distribution of mismatches across reads.
+    mismatch_profile.py -i $filename -l 150 -o ../QC/rseqc/${filename}
+
+    # Calculate the distributions of inserted nucleotides across reads
+    insertion_profile.py -i $filename -o .../QC/rseqc/${filename} -s "PE"
+
+    # read duplication sequence vs. mapping based
+    read_duplication.py -i $filename -o ../QC/rseqc/${filename}
+
+    # need BED format for this !!
+    # Calculate inner distance between read pairs.
+    inner_distance.py -i $filename -o ../QC/rseqc/${filename} -r /faststorage/project/ostrich_thermal/BACKUP/ostrich_reference/Struthio_camelus_HiC/Struthio_camelus_HiC_augustus.bed
+
+    # this program will compare detected splice junctions to reference gene model
+    # splicing annotation is performed in two levels: splice event level and splice junction level.
+    junction_annotation.py -i $filename -o ../QC/rseqc/${filename} -r /faststorage/project/ostrich_thermal/BACKUP/ostrich_reference/Struthio_camelus_HiC/Struthio_camelus_HiC_augustus.bed
+
+    # read distribution over genome features like exons, introns etc.
+    read_distribution.py -i $filename -r /faststorage/project/ostrich_thermal/BACKUP/ostrich_reference/Struthio_camelus_HiC/Struthio_camelus_HiC_augustus.bed > ../QC/rseqc/${filename}_dist.txt
+
+    FPKM_count.py -i $filename -o ../QC/rseqc/${filename} -r /faststorage/project/ostrich_thermal/BACKUP/ostrich_reference/Struthio_camelus_HiC/Struthio_camelus_HiC_augustus.bed
+done
