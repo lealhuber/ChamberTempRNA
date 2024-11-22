d5f40617b136fb5730d8787f554f33a49652f38c
lealhuber <lea.huber@datazug.ch>
2024-11-22 15:37:38 +0100

    Modified to count reads in big sample L38

--- scripts/countreads.sh
+++ scripts/countreads.sh
@@ -6,7 +6,7 @@
 
 #run in steps/mapping
 
-for filename in L7[6-9].Aligned.sortedByCoord.out.bam; do
+for filename in mapped/L38.Aligned.sortedByCoord.q255.out.bam; do
     ## index (if not already done)
     if ! test -f ${filename}.bai; then
         samtools index -@ 4 $filename
@@ -16,46 +16,15 @@ for filename in L7[6-9].Aligned.sortedByCoord.out.bam; do
     # format bam, stranded no, ordered by position (not by name)
     htseq-count -f bam -s no -r pos $filename \
         /faststorage/project/ostrich_thermal/BACKUP/ostrich_reference/Struthio_camelus_HiC/Struthio_camelus_HiC_augustus.gff \
-        > $filename.count_matrix.txt
-    echo $filename
+        > counts/L38.count_matrix.txt
     echo "htseq-count done"
 done
 
-for filename in L7.Aligned.sortedByCoord.out.bam; do
-    ## index (if not already done)
-    if ! test -f ${filename}.bai; then
-        samtools index -@ 4 $filename
-    fi
-    ## Count the reads
-    # default mode is union and noneunique mode none
-    # format bam, stranded no, ordered by position (not by name)
-    htseq-count -f bam -s no -r pos $filename \
-        /faststorage/project/ostrich_thermal/BACKUP/ostrich_reference/Struthio_camelus_HiC/Struthio_camelus_HiC_augustus.gff \
-        > $filename.count_matrix.txt
-    echo $filename
-    echo "htseq-count done"
-done
-
-for filename in L[8-9]*.Aligned.sortedByCoord.out.bam; do
-    ## index (if not already done)
-    if ! test -f ${filename}.bai; then
-        samtools index -@ 4 $filename
-    fi
-    ## Count the reads
-    # default mode is union and noneunique mode none
-    # format bam, stranded no, ordered by position (not by name)
-    htseq-count -f bam -s no -r pos $filename \
-        /faststorage/project/ostrich_thermal/BACKUP/ostrich_reference/Struthio_camelus_HiC/Struthio_camelus_HiC_augustus.gff \
-        > $filename.count_matrix.txt
-    echo $filename
-    echo "htseq-count done"
-done
-
-# make R style file with filenames for DESeqDataSetFromHTSeqCount function
-ls *count_matrix.txt | sed 's/.*/"&",/' > matrixFileNames.txt
-# same with first part of sample names (ADJUST!!!)
-ls *count_matrix.txt | sed 's/\(L.\)\(.Aligned*\)/"\1",/' > SampleNames.txt
-# make file with just file names to copy them to local computer
-ls *count_matrix.txt > matrixFileNamesCopy.txt
+# # make R style file with filenames for DESeqDataSetFromHTSeqCount function
+# ls *count_matrix.txt | sed 's/.*/"&",/' > matrixFileNames.txt
+# # same with first part of sample names (ADJUST!!!)
+# ls *count_matrix.txt | sed 's/\(L.\)\(.Aligned*\)/"\1",/' > SampleNames.txt
+# # make file with just file names to copy them to local computer
+# ls *count_matrix.txt > matrixFileNamesCopy.txt
 
 exit 0
